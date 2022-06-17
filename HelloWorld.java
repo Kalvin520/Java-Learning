b311ee061c3a69f4e374fcb69d6de0dc791b4074
Qing_qing <63888141+QingQing-520@users.noreply.github.com>
2022-06-17 23:47:27 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    // 1.console function
+    public static void main(String[] args) {
+        // 2.out put hello World
+        System.out.println("Hello World");
+    }
+}
