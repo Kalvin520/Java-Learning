6ca6adacaf3d2870978a19a99e8ed16dd1bac9e2
Qing_qing <63888141+QingQing-520@users.noreply.github.com>
2022-06-17 23:48:39 +0800

    Create Varaible.java

---
+++ Varaible.java
@@ -0,0 +1,10 @@
+public class Variable {
+    // 1.console function
+    public static void main(String[] args) {
+        // 2.variable declaration
+        // String
+        String myName = "Duck";
+        // Int
+        int myAge = 24;
+    }
+}
