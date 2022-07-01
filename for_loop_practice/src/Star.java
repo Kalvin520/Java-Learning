6a903ab1633b7cc3166d8b32727a1c3f401938bf
QingQing520 <clearletter520@gmail.com>
2022-07-01 12:34:38 +0800

    add star

---
+++ for_loop_practice/src/Star.java
@@ -0,0 +1,10 @@
+public class Star {
+    public static void main(String[] args) {
+        for (int i=1;i<=6;i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
