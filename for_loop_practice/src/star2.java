7982fa7c94fd5332c4aee8f636521cc7e463e304
QingQing520 <clearletter520@gmail.com>
2022-07-02 16:50:53 +0800

    renwe forloop

---
+++ for_loop_practice/src/star2.java
@@ -0,0 +1,11 @@
+public class star2 {
+    public static void main(String[] args) {
+        for (int i=1;i<=6;i++){
+            for (int j=1;j<=6-i+1;j++){
+               System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
