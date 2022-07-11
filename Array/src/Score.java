bad0d37c0bcf8c79b7feef5f201458a431c934df
Qing Qing <yaojaikai520@gmail.com>
2022-07-11 21:44:08 +0800

    array score renew

--- Array/src/Score.java
+++ Array/src/Score.java
@@ -8,15 +8,15 @@ public class Score {
                 {4, 66, 77, 88, 0, 0, 0},
                 {5, 22, 33, 66, 0, 0, 0,},
                 {0, 0, 0, 0, 0, 0, 0}};
+        //Array [x][y]=[列(由上往下)][行(由左至右)]
 
         final int student = 5; //student have 5 person
 
         for (int j = 1; j <= student; j++) {
             for (int k = 1; k <=3; k++) {
-
-                //score[j][4]=score[j][4]+score[j][k];
-                System.out.println(score[j][4]);
+                score[j][4]=score[j][4]+score[j][k];//calculate the average sum
             }
+            score[j][5]=score[j][4]/3;//calculate the average
         }
     }
 }
