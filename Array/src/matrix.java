af4f566f2fe09a2b1bce3a12dbb2ea206cc2eb54
Qing Qing <yaojaikai520@gmail.com>
2022-07-12 15:47:42 +0800

    add matrix

---
+++ Array/src/matrix.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class report {
+    public static void main(String[] args) {
+        int a[][]={ {-1,0,3},
+                    {2,1,-2}};
+
+        int b[][]={ {1,0},
+                    {2,-3},
+                    {-1,4}};
+
+        int m=2,p=2,n=3;
+
+        int c[][]=new int[m][p];
+
+    }
+}
