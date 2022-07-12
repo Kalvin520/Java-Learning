76e593fd6a8672d09bfb4008d7dc6e0d02be6709
Qing Qing <yaojaikai520@gmail.com>
2022-07-12 16:34:57 +0800

    add matrix

--- Array/src/matrix.java
+++ Array/src/matrix.java
@@ -1,5 +1,4 @@
-import java.util.*;
-public class report {
+public class matrix {
     public static void main(String[] args) {
         int a[][]={ {-1,0,3},
                     {2,1,-2}};
@@ -12,5 +11,40 @@ public class report {
 
         int c[][]=new int[m][p];
 
+        //print A matrix
+        System.out.println("A matrix is: ");
+        for (int i=0;i<m;i++){ //2
+            for (int j=0;j<n;j++) {//3
+                System.out.print(a[i][j]+ "  ");
+            }
+            System.out.println();
+        }
+
+        //print B matrix
+        System.out.println("B matrix is: ");
+        for (int i=0;i<n;i++){//3
+            for (int j=0;j<p;j++){//2
+                System.out.print(b[i][j]+ "  ");
+            }
+            System.out.println();
+        }
+
+        //matrix multiply
+        for (int i=0;i<m;i++){
+            for (int j=0;j<p;j++){
+                c[i][j]=0;
+                for (int k=0;k<n;k++)
+                    c[i][j]=c[i][j]+a[i][k]*b[k][j];
+            }
+        }
+
+        //print C matrix
+        System.out.println("A-matrix Multiply B-matrix= ");
+        for (int i=0;i<m;i++){
+            for (int j=0;j<p;j++){
+                System.out.print(c[i][j]+ "  ");
+            }
+            System.out.println();
+        }
     }
 }
