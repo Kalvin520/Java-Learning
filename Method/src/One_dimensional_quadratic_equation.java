1099ead4623f6dad61cfbd8143e444e4097e0e28
Qing Qing <yaojaikai520@gmail.com>
2022-07-14 22:20:00 +0800

    renew

--- Method/src/One_dimensional_quadratic_equation.java
+++ Method/src/One_dimensional_quadratic_equation.java
@@ -1,14 +1,15 @@
 public class One_dimensional_quadratic_equation {
-    static void equ(int a,int b,int c,double d[]) {
-        double e=Math.sqrt(b*b-4*a*c);
-        d[0]=(-b+e)/(2*a);
-        d[1]=(-b-e)/(2*a);
-    }
-    public static void main(String[] args) {
-        int a=2,b=-7,c=3;
-        double d[]=new double[2];
-        equ(a,b,c,d);
-        System.out.println(d[0]);
-        System.out.println(d[1]);
-    }
+        static void equ(int a,int b,int c,double d[]) {
+            double e=Math.sqrt(b*b-4*a*c);
+            d[0]=(-b+e)/(2*a);
+            d[1]=(-b-e)/(2*a);
+        }
+        public static void main(String[] args) {
+            int a=2,b=-7,c=3;
+            double d[]=new double[2];
+            equ(a,b,c,d);
+            System.out.println(d[0]);
+            System.out.println(d[1]);
+        }
+
 }
