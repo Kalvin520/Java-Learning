f7f86e32656ee466a937fcf6e62a333bb4dece4b
Qing_qing <63888141+QingQing-520@users.noreply.github.com>
2022-07-16 19:40:23 +0800

    Create Encapsulation_Practice.java

---
+++ class/src/Encapsulation_Practice.java
@@ -0,0 +1,32 @@
+class Pass3{
+    private int score;
+    public String result;
+    public boolean setscore(int a){
+        if (a>=0&&a<=100){
+            score=a;
+        return true;
+        }
+        else{
+            System.out.println("Input Error");
+            return false;
+        }
+    }
+
+    public void dispose(){
+        result="failed";
+        if (score>=60)
+            result="Pass";
+    }
+}
+
+
+public class Encapsulation_Practice {
+    public static void main(String[] args) {
+        Pass3 ps3=new Pass3();
+        boolean c=ps3.setscore(60);
+        if (c){
+            ps3.dispose();
+            System.out.println(ps3.result);
+        }
+    }
+}
