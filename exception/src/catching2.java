0ac0f1115710b3b3d5def0e0144650b4676a851b
Qing Qing <yaojaikai520@gmail.com>
2022-07-28 19:40:32 +0800

    add exception2

--- exception/src/catching2.java
+++ exception/src/catching2.java
@@ -1,5 +1,49 @@
 public class catching2 {
     public static void main(String[] args) {
+        try {
+            int a=6;
+            int b=0;
+            System.out.println(a/b);
+            int[] c=new int[4];
+            c[5]=3;
+        }
+        catch (ArithmeticException exception){
+            System.out.println("1: "+exception.getMessage());
+            System.out.println("2: "+exception.toString());
+            System.out.println("3: "+exception.getLocalizedMessage());
+            System.out.println("4: ");
+            exception.printStackTrace();
+        }
+        catch (IndexOutOfBoundsException exception){
+            System.out.println("1: "+exception.getMessage());
+            System.out.println("2: "+exception.toString());
+            System.out.println("3: "+exception.getLocalizedMessage());
+            System.out.println("4: ");
+            exception.printStackTrace();
+        }
 
+        catch (RuntimeException exception){
+            System.out.println("1: "+exception.getMessage());
+            System.out.println("2: "+exception.toString());
+            System.out.println("3: "+exception.getLocalizedMessage());
+            System.out.println("4: ");
+            exception.printStackTrace();
+        }
+
+        catch (Exception exception){
+            System.out.println("1: "+exception.getMessage());
+            System.out.println("2: "+exception.toString());
+            System.out.println("3: "+exception.getLocalizedMessage());
+            System.out.println("4: ");
+            exception.printStackTrace();
+        }
+
+        catch (Throwable exception){
+            System.out.println("1: "+exception.getMessage());
+            System.out.println("2: "+exception.toString());
+            System.out.println("3: "+exception.getLocalizedMessage());
+            System.out.println("4: ");
+            exception.printStackTrace();
+        }
     }
 }
