68f93e8ef6dd727a62fcdc75acd07149ad1a1ff7
Qing Qing <yaojaikai520@gmail.com>
2022-08-09 21:49:34 +0800

    renew Data_Output_stream

--- File/src/Data_OutputStream.java
+++ File/src/Data_OutputStream.java
@@ -12,5 +12,11 @@ public class Data_OutputStream {
         String s1="stream test";
         d1.writeBytes(s1);
         d1.close();
+
+        FileWriter f2=new FileWriter("test3.txt");
+        BufferedWriter b1=new BufferedWriter(f2);
+        String s2="這是一本神奇的書";
+        b1.write(s2);
+        b1.close();
     }
 }
